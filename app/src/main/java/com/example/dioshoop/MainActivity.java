94fbd6711ca5247c3334e7dcc8df6915a18ef61a
JasmineAR985 <muhammadilhamnurhasani@gmail.com>
2021-02-08 08:32:34 +0700

    v3

--- app/src/main/java/com/example/dioshoop/MainActivity.java
+++ app/src/main/java/com/example/dioshoop/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.dioshoop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void chat(View view){
+        Intent intent=new Intent(MainActivity.this,ChatActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void history(View view){
+        Intent intent=new Intent(MainActivity.this,HistoryActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void home(View view){
+        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void checklist(View view){
+        Intent intent=new Intent(MainActivity.this,ChecklistActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void settings(View view){
+        Intent intent=new Intent(MainActivity.this,SettingsActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void shopping(View view){
+        Intent intent=new Intent(MainActivity.this,ShoppingActivity.class);
+        startActivity(intent);
+
+    }
+
 }
